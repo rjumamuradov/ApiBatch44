f5006af7b8de98c6a8dd2247b49ab859877cf05b
rjumamuradov <rozymyrat.jumamuradov@gmail.com>
2022-03-04 20:05:05 +0100

    ders y

---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class Authentication {
+
+    public static  String generateToken(){
+            String usarname="Batch44";
+            String password="Batch44+";
+        Map<String,Object> map=new HashMap<>();
+        map.put("Batch44",usarname);
+        map.put("Batch44+",password);
+        String endPoint = "http://www.gmibank.com/api/authenticate";
+        Response response = given().contentType(ContentType.JSON).body(map).when().post(endPoint);
+        JSON
+        return null;
+    }
+}
